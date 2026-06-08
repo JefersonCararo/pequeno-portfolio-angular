b6ea157c4d2f59f7ac54bc55b74a0701a62cc7ff
JefersonCararo <20241ctb0100055@estudantes.ifpr.edu.br>
2026-06-08 20:31:28 +0000

    Aula16: Cria componentes, navegacao com Material e rotas do Portfolio

--- portfolio-angular/src/app/app.ts
+++ portfolio-angular/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { Sobre } from './sobre/sobre';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('pequeno-portfolio-angular');
-}
+export class App {}
+
